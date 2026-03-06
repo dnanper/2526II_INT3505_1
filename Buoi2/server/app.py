a71b876f3d1409f3961ac427fc3e084f07911552
dnanper <anvocuc05@gmail.com>
2026-03-06 08:59:02 +0700

    Buoi2: ver2 - Uniform interface

--- Buoi2/server/app.py
+++ Buoi2/server/app.py
@@ -2,12 +2,30 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+students = {
+    "1": {"name": "An", "major": "IT"},
+    "2": {"name": "Bình", "major": "Design"}
+}
+
+classrooms = {
+    "A": {"room": 404},
+    "B": {"room": 306}
+}
+
 @app.route("/health", methods=["GET"])
 def health_check():
     return jsonify({
         "status": "ok",
         "message": "service is running"
     }), 200
+
+@app.route("/students", methods=["GET"])
+def get_all_students():
+    return jsonify(students), 200
+
+@app.route("/classrooms", methods=["GET"])
+def get_all_classrooms():
+    return jsonify(classrooms), 200
 
 if __name__ == "__main__":
     app.run(port=5000)
